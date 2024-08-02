70ceea8cb519ab4aec14f715fc7284b0e8eaac0f
javimp2003uma <javimp2003uma@uma.es>
2024-08-02 18:25:04 +0200

    changes

--- src/script.py
+++ src/script.py
@@ -43,6 +43,11 @@ def remove_empty_dirs(path):
         print()
 
 def removeAll():
+    """
+    Removes the dataset_split directory, dataset.zip file, and dataset directory if they exist.
+    Provides feedback if these items are not found.
+    """
+
     if os.path.exists(f"{cd}/dataset_split"):
         shutil.rmtree(f'{cd}/dataset_split', ignore_errors=True)
         print("Data distribution deleted successfully ...")
@@ -159,6 +164,13 @@ def train_model(yaml_file, epochs, project):
         f.write(" Recall: {}\n".format(results2.box.r))
 
 def inference_over_image(sourcePath):
+    """
+    Performs inference on a single image using a pre-trained YOLO model and prints the result.
+
+    Args:
+        sourcePath (str): Path to the image file to be analyzed.
+    """
+
     file_path = 'best.pt'
     urlModelDrive = 'https://drive.google.com/uc?id=1h7PrvmW8SaI6wyGE1x2bD-nMirccyfcr' # change URL
     gdown.download(urlModelDrive,file_path,quiet=False)
@@ -173,22 +185,116 @@ def inference_over_image(sourcePath):
         print(f"2 hands found with {results[0].boxes[0].conf} and {results[0].boxes[1].conf}")
         hand1 = results[0].boxes[0].cls
         hand2 = results[0].boxes[1].cls
-        print(f"Facing {hand1} vs {hand2}\U0001f600")
+        determine_winner(hand1,hand2)
 
-        if hand1 == hand2:
-            print("It's a draw!")
-            print(f"Both hands are {hand1}.")
-        elif (hand1 == "Rock" and hand2 == "Scissors") or \
-             (hand1 == "Scissors" and hand2 == "Paper") or \
-             (hand1 == "Paper" and hand2 == "Rock"):
-            print("Hand 1 wins!")
-            print(f"{hand1} beats {hand2} :)")
+
+def determine_winner(hand1, hand2):
+    """
+    Determines the winner of a rock-paper-scissors game based on the given hands and prints the result.
+
+    Args:
+        hand1 (str): The class of the first hand (e.g., 'Rock', 'Paper', 'Scissors').
+        hand2 (str): The class of the second hand (e.g., 'Rock', 'Paper', 'Scissors').
+    """
+
+    print(f"Facing {hand1} vs {hand2}\U0001f600")
+
+    if hand1 == hand2:
+        print("It's a draw!")
+        print(f"Both hands are {hand1}.")
+    elif (hand1 == "rock" and hand2 == "scissors") or \
+            (hand1 == "scissors" and hand2 == "paper") or \
+            (hand1 == "paper" and hand2 == "rock"):
+        print("Hand 1 wins!")
+        print(f"{hand1} beats {hand2} :)")
+    else:
+        print("Hand 2 wins!")
+        print(f"{hand2} beats {hand1} :)")
+
+
+def consistency(pairs):
+    """
+    Checks if all pairs in the list are consistent, meaning they match the first pair in either order.
+
+    Args:
+        pairs (list): List of tuples where each tuple represents a pair of classes.
+
+    Returns:
+        bool: True if all pairs are consistent, False otherwise.
+    """
+
+    first_pair = pairs[0]
+    for pair in pairs[1:]:
+        if not ((pair[0] == first_pair[0] and pair[1] == first_pair[1]) or
+                (pair[0] == first_pair[1] and pair[1] == first_pair[0])):
+            return False
+    return True
+
+
+def inference_over_video(sourcePath, frame_buffer_size=5):
+    """
+    Performs inference on a video file, analyzing pairs of detected hands across frames and determining the winner if pairs are consistent.
+
+    Args:
+        sourcePath (str): Path to the video file to be analyzed.
+        frame_buffer_size (int): Number of frames to buffer for consistency checking. Default is 5.
+    """
+
+    file_path = 'best.pt'
+    urlModelDrive = 'https://drive.google.com/uc?id=1h7PrvmW8SaI6wyGE1x2bD-nMirccyfcr'  # Cambia la URL si es necesario
+    gdown.download(urlModelDrive, file_path, quiet=False)
+
+    cap = cv2.VideoCapture(sourcePath)
+    model = YOLO(file_path)
+
+    # Buffer para almacenar pares de clases detectadas
+    frame_buffer = []
+
+    while cap.isOpened():
+        success, frame = cap.read()
+
+        if success:
+            # Ejecuta el seguimiento con YOLOv8 en el frame, persistiendo las pistas entre frames
+            results = model.track(frame, persist=True)
+
+            if results[0].boxes:
+                # Asegúrate de que haya exactamente 2 manos detectadas
+                if len(results[0].boxes) == 2:
+                    hand1_cls = results[0].boxes[0].cls
+                    hand2_cls = results[0].boxes[1].cls
+                    frame_buffer.append((hand1_cls, hand2_cls))
+
+                    if len(frame_buffer) > frame_buffer_size:
+                        frame_buffer.pop(0)
+
+                    # Verifica la consistencia después de llenar el buffer
+                    if len(frame_buffer) == frame_buffer_size:
+                        if consistency(frame_buffer):
+                            # Determina el ganador basado en el par consistente
+                            determine_winner(frame_buffer[0][0], frame_buffer[0][1])
+                            frame_buffer = []  # Reinicia el buffer después de determinar el ganador
+                        else:
+                            print("Las detecciones no son consistentes, esperando más frames...")
+                else:
+                    # Si no hay exactamente 2 manos detectadas, reinicia el buffer
+                    frame_buffer = []
+
+            # Visualiza los resultados en el frame
+            annotated_frame = results[0].plot()
+            cv2.imshow("YOLOv8 Tracking", annotated_frame)
+
+            # Rompe el bucle si se presiona 'q'
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
         else:
-            print("Hand 2 wins!")
-            print(f"{hand2} beats {hand1} :)")
+            # Rompe el bucle si se llega al final del video
+            break
 
-        
-    
+    # Libera el objeto de captura de video y cierra la ventana de visualización
+    cap.release()
+    cv2.destroyAllWindows()
+
+
 if len(sys.argv) == 2 and 'c' in sys.argv:
     prepare_structure()
 elif len(sys.argv) == 3 and 't' in sys.argv:
@@ -201,11 +307,19 @@ elif len(sys.argv) == 3 and 't' in sys.argv:
         print(f"Invalid argument format: {sys.argv[-1]}  :( Expected format: --e=nEpochs...")
 elif len(sys.argv) == 2 and 'r' in sys.argv:
     removeAll()
-elif len(sys.argv) == 3 and 'i' in sys.argv:
+elif len(sys.argv) == 3 and 'ii' in sys.argv:
     if sys.argv[-1].startswith("--source="):
         # Remove "--source=" prefix
         sourcePath = int(sys.argv[-1][9:])
         inference_over_image(sourcePath)
+    else:
+        # Handle invalid argument format
+        print(f"Invalid argument format: {sys.argv[-1]}  :( Expected format: --source=sourcePath...")
+elif len(sys.argv) == 3 and 'iv' in sys.argv:
+    if sys.argv[-1].startswith("--source="):
+        # Remove "--source=" prefix
+        sourcePath = int(sys.argv[-1][9:])
+        inference_over_video(sourcePath)
     else:
         # Handle invalid argument format
         print(f"Invalid argument format: {sys.argv[-1]}  :( Expected format: --source=sourcePath...")
